22abc6ee1a9cfaa1a76f248478631b6417ec7628
dongjize <justecommeca@163.com>
2018-08-06 23:51:37 +0800

    update data

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 with open("wiki_misspell.txt", "r") as f:
     for word in f.readlines():
+        # generate wiki_rectified
         pass
+
+# with open("wiki_correct.txt", "r") as f:
+#     for i in range(len(f.readlines())):
+#         pass
