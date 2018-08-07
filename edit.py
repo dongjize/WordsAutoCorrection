c1cd57cd020d93dde25247b83fef473055b1293e
Dong <jason.dong@sap.com>
2018-08-07 21:03:07 +0800

    temp

---
+++ edit.py
@@ -0,0 +1,7 @@
+import re
+
+misspell = open("wiki_misspell.txt", "r")
+rectified = open("wiki_misspell.txt", "w")
+
+for word in misspell:
+    pass
--- ngram.py
+++ ngram.py
@@ -1,6 +1,5 @@
+import re
+
 with open("wiki_misspell.txt", "r") as f:
     for word in f.readlines():
         pass
-
-precision = 0
-recall = 0
